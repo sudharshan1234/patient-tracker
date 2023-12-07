d97d4f93875cdee5e3595168ca54f120efad330f
Hemangani Nagarajan <nhema2231@gmail.com>
2023-12-06 19:41:11 -0500

    added appointment modification backend code

--- patient-tracker-back/routes/appointmentRoutes.js
+++ patient-tracker-back/routes/appointmentRoutes.js
@@ -4,8 +4,11 @@ const appointmentController = require('../controllers/appointmentController');
 
 
 router.get('/', appointmentController.getAllAppointments);
+router.get('/:id', appointmentController.getAppointmentById);
 router.post('/', appointmentController.scheduleAppointment);
-router.get('/:doctorId', appointmentController.getAppointmentsByDoctor);
+router.get('/doctor/:doctorId', appointmentController.getAppointmentsByDoctor);
+router.put('/:id', appointmentController.updateAppointment);
+router.delete('/:id', appointmentController.deleteAppointment);
 
 
 module.exports = router;
