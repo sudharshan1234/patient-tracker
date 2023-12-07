e3dd4822584f9af03c871c2ef82393dde1315730
SmruthiSathyamoorthy <smruthi.sathyamoorthy@gmail.com>
2023-12-06 19:59:21 -0500

    added token verification, add patient for doctor

--- patient-tracker-back/routes/doctorRoutes.js
+++ patient-tracker-back/routes/doctorRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const doctorController = require('../controllers/doctorController');
+const { verifyToken } = require('../jwt-middleware');
+
+
 
 
 router.post('/register', doctorController.registerDoctor);
 router.post('/login', doctorController.loginDoctor);
-router.get('/patients/:doctorId', doctorController.getPatientDetailsForDoctor);
+router.get('/patients/:doctorId', verifyToken, doctorController.getPatientDetailsForDoctor);
+router.post('/patients/:doctorId', verifyToken, doctorController.addPatientForDoctor);
+
 
 module.exports = router;
